5944510ba31a1479f43166da8bd743ee9df5fc84
H B <phajib@gmail.com>
2018-10-26 06:37:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+//shout function
+function shout(string){
+  return string.toUpperCase()
+}
+
+//whisper function
+function whisper(string){
+  return string.toLowerCase()
+}
+
+//logShout function
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+//logWhisper function
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+//sayHiToGrandma function
+function sayHiToGrandma(string){
+var lowercase = "hello"
+  if(lowercase.toLowerCase() === lowercase)
+  {
+    console.log('I can\'t hear you')
+    return lowercase.toLowerCase()
+  }
+    console.log('hello')
+
+var uppercase = `string`
+  if (uppercase.toUpperCase() === uppercase){
+    console.log('YES INDEED!')
+  }
+
+  if (`string` === 'I love you, Grandma'){
+      console.log("I love you too")
+  }
+}
